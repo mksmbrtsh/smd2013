1f9cd01b620363edf7f4cee79e77ccede203465d
mksmbrtsh <mksmbrtsh@gmail.com>
2014-01-03 22:14:42 +0400

    add hint and key search

--- src/com/maximsblog/blogspot/com/smd2013/MainActivity.java
+++ src/com/maximsblog/blogspot/com/smd2013/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
@@ -28,59 +29,74 @@ import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.widget.SearchView;
 
 import android.view.MotionEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends SherlockFragmentActivity {
 
-
-    private EditText mEditFilter;
+	private EditText mEditFilter;
 	private String mFilter;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
 		OpenDBHelper mDBh = new OpenDBHelper(this);
-		if(savedInstanceState!=null)
+		if (savedInstanceState != null)
 			mFilter = savedInstanceState.getString("filter");
-		else 
+		else
 			mFilter = "";
-    }
-    
-    @Override
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getSupportMenuInflater().inflate(R.menu.menu, menu);
 		menu.getItem(0).setActionView(R.layout.search_title);
-		mEditFilter = (EditText) menu.getItem(0).getActionView().findViewById(R.id.search_editText);
+		mEditFilter = (EditText) menu.getItem(0).getActionView()
+				.findViewById(R.id.search_editText);
 		mEditFilter.setText(mFilter);
-        return super.onCreateOptionsMenu(menu);
+		mEditFilter.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+		mEditFilter.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+		    @Override
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+		        	CatalogFragment cf = (CatalogFragment) getSupportFragmentManager()
+							.findFragmentById(R.id.fragment1);
+					cf.setFilter(mEditFilter.getText().toString());
+		            return true;
+		        }
+		        return false;
+		    }
+		});
+		return super.onCreateOptionsMenu(menu);
 	}
-    
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-    	if(item.getItemId() == R.id.about_item)	{
-    		AboutFragment about = new AboutFragment();
-    		about.show(getSupportFragmentManager(), "about");
-    	} else if(item.getItemId() == R.id.menu_next_search){
-    		CatalogFragment cf= (CatalogFragment) getSupportFragmentManager().findFragmentById(R.id.fragment1);
-    		cf.setFilter(mEditFilter.getText().toString());
-    	} else if(item.getItemId() == R.id.pdf_path) {
-    		SharedPreferences preferences = PreferenceManager
-    				.getDefaultSharedPreferences(this);
-    		String pdfpath = preferences.getString("pdfpath", getCacheDir().getAbsolutePath());
-    		Intent intent = new Intent(this,
-					FileDialog.class);
-    		intent.putExtra(FileDialog.START_PATH, pdfpath);
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		if (item.getItemId() == R.id.about_item) {
+			AboutFragment about = new AboutFragment();
+			about.show(getSupportFragmentManager(), "about");
+		} else if (item.getItemId() == R.id.menu_next_search) {
+			CatalogFragment cf = (CatalogFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.fragment1);
+			cf.setFilter(mEditFilter.getText().toString());
+		} else if (item.getItemId() == R.id.pdf_path) {
+			SharedPreferences preferences = PreferenceManager
+					.getDefaultSharedPreferences(this);
+			String pdfpath = preferences.getString("pdfpath", getCacheDir()
+					.getAbsolutePath());
+			Intent intent = new Intent(this, FileDialog.class);
+			intent.putExtra(FileDialog.START_PATH, pdfpath);
 			intent.putExtra(FileDialog.CAN_SELECT_DIR, true);
-			intent.putExtra(FileDialog.SELECTION_MODE,
-					SelectionMode.MODE_OPEN);
+			intent.putExtra(FileDialog.SELECTION_MODE, SelectionMode.MODE_OPEN);
 			startActivityForResult(intent, 777);
-			
-    	}
-    	return super.onMenuItemSelected(featureId, item);
-    }
-    
-    @Override
+
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (data != null && requestCode == 777 && resultCode == RESULT_OK) {
 			File f = new File(data.getStringExtra(FileDialog.RESULT_PATH));
@@ -91,12 +107,12 @@ public class MainActivity extends SherlockFragmentActivity {
 			editor.commit();
 		}
 	}
-   
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-    	if(mEditFilter!=null)
-    		outState.putString("filter", mEditFilter.getText().toString());
-    	super.onSaveInstanceState(outState);
-    }
-    
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		if (mEditFilter != null)
+			outState.putString("filter", mEditFilter.getText().toString());
+		super.onSaveInstanceState(outState);
+	}
+
 }
